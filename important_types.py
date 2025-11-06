aedbafa6dcf8b79056a6cbcdb299bdf110b5f330
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-06 16:43:39 -0300

    feat: added new types

--- important_types.py
+++ important_types.py
@@ -1,6 +1,11 @@
 from node import Node  
 
 # like a matrix
+NodeNeighbors = list[str]
 NodeList = dict[str, list[str]]
+NodeOutDegree = int
+NodeInDegree = int
+NodeTotal = NodeOutDegree | NodeInDegree
 NodeConnections = list[Node]
 Path = list[str]
+Degree = dict[str, dict[str, NodeTotal]]
